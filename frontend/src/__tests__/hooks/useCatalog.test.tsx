d4c681824851dcd3f3de956132aed18a9ac96c46
remyShift <remy.engassac@icloud.com>
2025-07-14 20:47:43 +0200

    (red) add initial test for useCatalog hook to start with a loading on true

---
+++ frontend/src/__tests__/hooks/useCatalog.test.tsx
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { useCatalog } from '../../hooks/useCatalog';
+
+describe('useCatalog', () => {
+    it('should start with loading=true', async () => {
+      const { result } = renderHook(() => useCatalog());
+      expect(result.current.loading).toBe(true);
+    });
+  }
+);
