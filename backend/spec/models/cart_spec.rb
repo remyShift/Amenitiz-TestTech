0b43d76846c882b01461cbd3287d8bfbc747b57a
remyShift <remy.engassac@icloud.com>
2025-07-14 18:33:28 +0200

    (red) add API test for total price calculation based on item codes in the cart

--- backend/spec/models/cart_spec.rb
+++ backend/spec/models/cart_spec.rb
@@ -127,3 +127,11 @@ RSpec.describe Cart, type: :model do
         end
     end
 end
+
+RSpec.describe "Cart API", type: :request do
+    it "returns the total price for a given list of codes" do
+        post "/cart/total", params: { items: ["GR1", "GR1"] }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)["total"]).to eq(3.11)
+    end
+end
