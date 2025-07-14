d886c563f2d6a477963f41b855336646a087e9d5
remyShift <remy.engassac@icloud.com>
2025-07-14 20:29:38 +0200

    (refactor) remove app initial display

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, type UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react({
+			jsxRuntime: 'automatic',
+		}),
+	],
+	test: {
+		environment: 'jsdom',
+	},
+} as UserConfig);
