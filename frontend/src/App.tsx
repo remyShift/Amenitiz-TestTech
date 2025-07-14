0e9b6a302840e32c062b33a33c2813098ae803e1
remyShift <remy.engassac@icloud.com>
2025-07-14 20:34:04 +0200

    (config) add Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello World</h1>
+    </div>
   )
 }
-
-export default App
