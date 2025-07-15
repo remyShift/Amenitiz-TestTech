510027ac20088716c171ce4ee93ed32c69b5f972
remyShift <remy.engassac@icloud.com>
2025-07-15 11:21:07 +0200

    (red) check if on mount my Catalog component call the fetch catalog function from the hook

--- frontend/src/__tests__/components/Catalog.test.tsx
+++ frontend/src/__tests__/components/Catalog.test.tsx
@@ -1,11 +1,23 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { act, render, renderHook, screen } from '@testing-library/react';
 import Catalog from '@/components/Catalog';
+import { useCatalog } from '@/hooks/useCatalog';
 
 describe('Catalog', () => {
     it('should render the catalog', () => {
         render(<Catalog />);
 
         expect(screen.getByTestId('catalog')).toBeDefined();
+    });
+
+    it('should call the fetchCatalog function when the component is mounted and set loading to false when the catalog is fetched', async () => {
+        const { result } = renderHook(() => useCatalog());
+        render(<Catalog />);
+
+        expect(screen.getByTestId('catalog')).toBeDefined();
+
+        await act(async () => result.current.fetchCatalog());
+
+        expect(result.current.loading).toBe(false);
     });
 });
