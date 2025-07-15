0d7ebda9ba23304993664dec51d6df9fbe024ce2
remyShift <remy.engassac@icloud.com>
2025-07-15 14:57:54 +0200

    (red) add unit test for useCart hook to verify item addition functionality

---
+++ frontend/src/__tests__/hooks/useCart.test.tsx
@@ -0,0 +1,14 @@
+import { useCart } from "@/hooks/useCart";
+import { renderHook } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+describe('useCart', () => {
+    it('should add an item to the cart when the addItem function is called', () => {
+        const itemToAdd = { id: 1, code: '123', name: 'Coffee', price: 10 };
+        const { result } = renderHook(() => useCart());
+
+        result.current.addItem(itemToAdd);
+
+        expect(result.current.items).toEqual([itemToAdd]);
+    });
+});
