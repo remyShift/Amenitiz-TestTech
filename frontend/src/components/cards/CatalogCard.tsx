4aca91e38a678eb921a9dc01fdf3b15488d0c32d
remyShift <remy.engassac@icloud.com>
2025-07-15 14:55:14 +0200

    (refactor) enhance CatalogCard component to display item details separately for improved readability

--- frontend/src/components/cards/CatalogCard.tsx
+++ frontend/src/components/cards/CatalogCard.tsx
@@ -3,9 +3,15 @@ import type { CatalogItem } from "@/types/Catalog";
 export default function CatalogCard({ item }: { item: CatalogItem }) {
     return (
         <div data-testid="catalog-card">
-            {/* <p className="text-lg font-bold text-blue-600">
-                {item.code} | {item.name} | {item.price}
-            </p> */}
+            <p className="text-lg font-bold text-blue-600">
+                {item.code}
+            </p>
+            <p className="text-lg font-bold text-blue-600">
+                {item.name}
+            </p>
+            <p className="text-lg font-bold text-blue-600">
+                {item.price}€
+            </p>
         </div>
     )
 }
