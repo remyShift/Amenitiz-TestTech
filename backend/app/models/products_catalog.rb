4919b7b805d0b2488a6e2531e820b01ca7b47337
remyShift <remy.engassac@icloud.com>
2025-07-16 11:04:50 +0200

    red: add test to introduc in product catalog search through the db instead of hard data

--- backend/app/models/products_catalog.rb
+++ backend/app/models/products_catalog.rb
@@ -1,3 +1,17 @@
 class ProductsCatalog
+    PRODUCTS = {
+        "GR1" => { name: "Green Tea", price: 3.11 },
+        "SR1" => { name: "Strawberries", price: 5.00 },
+        "CF1" => { name: "Coffee", price: 11.23 }
+    }
 
+    def self.find(code)
+        PRODUCTS[code]
+    end
+
+    def self.all
+        PRODUCTS.map do |code, data|
+            { code: code, name: data[:name], price: data[:price] }
+        end
+    end
 end
